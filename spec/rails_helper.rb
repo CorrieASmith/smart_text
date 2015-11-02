4d55ff12e9414ae44efa17659a85d9154727594b
CorrieASmith <corriesmith87@gmail.com>
2015-11-02 11:43:58 -0800

    add migration and model for contacts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,3 +50,7 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+VCR.config do |c|
+  c.ignore_localhost = true
+end
