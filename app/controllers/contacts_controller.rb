0b8a8278eaa173c070403796efa74c566fa42a3a
CorrieASmith <corriesmith87@gmail.com>
2015-11-02 12:03:43 -0800

    add contact model with RESTful routes

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,48 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+   @contact = Contact.find(params[:id])
+ end
+
+ def new
+   @contact = Contact.new
+   render :new
+ end
+
+ def create
+   @contact = Contact.new(contact_params)
+   if @contact.save
+     flash[:notice] = "Contact saved!"
+     redirect_to "/"
+   else
+     render :new
+   end
+ end
+
+ def edit
+   @contact = Contact.find(params[:id])
+   render :edit
+ end
+
+ def update
+   @contact = Contact.find(params[:id])
+   if @contact.update(contact_params)
+     redirect_to "/"
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @contact = Contact.find(params[:id])
+   @contact.destroy
+   redirect_to "/"
+ end
+
+ private def contact_params
+   params.require(:contact).permit(:name, :number)
+ end
+end
