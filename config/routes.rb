3ab51c4a72238c327f2241a364052732127a0ead
CorrieASmith <corriesmith87@gmail.com>
2015-11-03 09:19:10 -0800

    add login info to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => "home#index"
+  resources :users
   resources :messages, :only => [:index, :new, :create, :show]
   resources :contacts
 end
